0c399466a304d3abbed8ed6442cf7612043e5f1d
Jakub Jenc <jajenc@seznam.cz>
2020-03-02 03:57:46 +0100

    Update to CRUD project

--- java-test-default/src/main/java/com/etnetera/hr/controller/JavaScriptFrameworkController.java
+++ java-test-default/src/main/java/com/etnetera/hr/controller/JavaScriptFrameworkController.java
@@ -1,31 +1,67 @@
 package com.etnetera.hr.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.etnetera.hr.data.JavaScriptFramework;
 import com.etnetera.hr.repository.JavaScriptFrameworkRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 /**
  * Simple REST controller for accessing application logic.
- * 
- * @author Etnetera
  *
+ * @author Etnetera
  */
 @RestController
 public class JavaScriptFrameworkController extends EtnRestController {
 
-	private final JavaScriptFrameworkRepository repository;
+    private final JavaScriptFrameworkRepository repository;
 
-	@Autowired
-	public JavaScriptFrameworkController(JavaScriptFrameworkRepository repository) {
-		this.repository = repository;
-	}
+    @Autowired
+    public JavaScriptFrameworkController(JavaScriptFrameworkRepository repository) {
+        this.repository = repository;
+    }
 
-	@GetMapping("/frameworks")
-	public Iterable<JavaScriptFramework> frameworks() {
-		return repository.findAll();
-	}
+    @GetMapping("/frameworks")
+    public Iterable<JavaScriptFramework> frameworks() {
+        return repository.findAll();
+    }
+
+    @GetMapping("frameworks/{frameworkId}")
+    public Optional<JavaScriptFramework> framework(@PathVariable long frameworkId) {
+        return repository.findById(frameworkId);
+    }
+
+    @PostMapping("/frameworks")
+    public JavaScriptFramework saveFramework(String name, double version, LocalDate deprecationDate, int hypeLevel) {
+        JavaScriptFramework frameworkToSave = new JavaScriptFramework(name, version, deprecationDate, hypeLevel);
+        repository.save(frameworkToSave);
+        return frameworkToSave;
+    }
+
+    @PutMapping("/frameworks/{frameworkId}")
+    public JavaScriptFramework updateFramework(@PathVariable long frameworkId, String name, double version,
+                                               LocalDate deprecationDate, int hypeLevel, JavaScriptFramework frameworkToUpdate) throws Exception{
+        if (!repository.existsById(frameworkId)) {
+            throw new NotFoundException("Framework has to exist in the database!");
+        }
+        //JavaScriptFramework frameworkToUpdate = repository.findById(frameworkId);
+        frameworkToUpdate.setName(name);
+        frameworkToUpdate.setVersion(version);
+        frameworkToUpdate.setDeprecationDate(deprecationDate);
+        frameworkToUpdate.setHypeLevel(hypeLevel);
+        repository.save(frameworkToUpdate);
+        return frameworkToUpdate;
+    }
+
+    @DeleteMapping("/frameworks/{frameworkId}")
+    public void deleteFramework(@PathVariable long frameworkId){
+        repository.deleteById(frameworkId);
+    }
 
 }
--- java-test-default/src/main/java/com/etnetera/hr/data/JavaScriptFramework.java
+++ java-test-default/src/main/java/com/etnetera/hr/data/JavaScriptFramework.java
@@ -1,53 +1,94 @@
 package com.etnetera.hr.data;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 /**
  * Simple data entity describing basic properties of every JavaScript framework.
- * 
- * @author Etnetera
  *
+ * @author Etnetera
  */
 @Entity
 public class JavaScriptFramework {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	@Column(nullable = false, length = 30)
-	private String name;
+    @Column(nullable = false, length = 30)
+    private String name;
 
-	public JavaScriptFramework() {
-	}
+    @Column(nullable = false, length = 6)
+    private double version;
 
-	public JavaScriptFramework(String name) {
-		this.name = name;
-	}
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate deprecationDate;
 
-	public Long getId() {
-		return id;
-	}
+    private int hypeLevel;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public JavaScriptFramework() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public JavaScriptFramework(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public JavaScriptFramework(String name, double version, LocalDate deprecationDate, int hypeLevel) {
+        this.name = name;
+        this.version = version;
+        this.deprecationDate = deprecationDate;
+        this.hypeLevel = hypeLevel;
+    }
 
-	@Override
-	public String toString() {
-		return "JavaScriptFramework [id=" + id + ", name=" + name + "]";
-	}
+    public double getVersion() {
+        return version;
+    }
+
+    public void setVersion(double version) {
+        this.version = version;
+    }
+
+    public LocalDate getDeprecationDate() {
+        return deprecationDate;
+    }
+
+    public void setDeprecationDate(LocalDate deprecationDate) {
+        this.deprecationDate = deprecationDate;
+    }
+
+    public int getHypeLevel() {
+        return hypeLevel;
+    }
+
+    public void setHypeLevel(int hypeLevel) {
+        this.hypeLevel = hypeLevel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "JavaScriptFramework [id=" + id + ", name=" + name + "]";
+    }
 
 }
